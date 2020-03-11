65a2dc5881afd69999cca65acc6803fe2079b9d1
Aldrumont <ajunior@quickium.com>
2020-03-11 16:13:11 -0300

    fim -

--- track_finder.py
+++ track_finder.py
@@ -1,14 +1,19 @@
 import cv2
 import os
-
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
 caminho_imagem = "fotos/curva/frame_1_0.518295454545.jpg"
 
+parser = argparse.ArgumentParser(description='Description of your program')
+parser.add_argument('-p','--n_pontos', help='numero de pontos', type=int, required=False)
+parser.add_argument('-l','--tamanho_linha', help='tamanho da linha', type=int, required=False)
+parser.add_argument('-a','--alpha', help='nivel transparencia', type=float, required=False)
+args = vars(parser.parse_args())
 
-def processamento_imagem(caminho_imagem, qtd_pontos=10, salva_imagem="img.jpg"):
+def processamento_imagem(caminho_imagem, qtd_pontos=10, tamanho_linha=10, alpha = 1, salva_imagem="img.jpg" ):
     """Funcao que aplica todos os métodos de processamentos descrito na metodologia do TCC
     
     Argumentos:
@@ -18,14 +23,15 @@ def processamento_imagem(caminho_imagem, qtd_pontos=10, salva_imagem="img.jpg"):
         qtd_pontos {int} -- quantidade de pontos (default: {10})
         salva_imagem {string} -- caminho que deseja salvar imagem (exemplo: /Documents/img.jpg) 
     """
-
+   
     img_original = cv2.imread(caminho_imagem) # método carrega uma imagem de um arquivo.
     img_cinza = cv2.cvtColor(img_original,cv2.COLOR_BGR2GRAY) #conversão imagem BGR para escala cinza
     img_suavizada = cv2.medianBlur(img_cinza,21) #Suavização dos ruídos
     _, img_binaria = cv2.threshold(img_suavizada, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU) #Aplicação do Threshold
     
     pontos_trajetoria, pontos_centrais = pega_pontos_trajetoria(img_binaria, qtd_pontos)
-    desenha_imagem(img_original, pontos_trajetoria, salva_imagem)
+    # plota_pontos(img_original, pontos_trajetoria, salva_imagem)
+    desenha_faixa(img_original,pontos_trajetoria,"img.jpg",tamanho_linha, alpha)
 
 def pega_pontos_trajetoria(img_binaria, qtd_pontos=10):
     """Funcao que pega os pontos da trajetoria
@@ -53,7 +59,7 @@ def pega_pontos_trajetoria(img_binaria, qtd_pontos=10):
         pontos_centrais.append((largura//2,(altura_da_parte)*ponto+altura_da_parte//2))
     return (pontos_trajetoria,pontos_centrais)
 
-def desenha_imagem(img, pontos_trajetoria, salva_imagem):
+def plota_pontos(img, pontos_trajetoria, salva_imagem):
     """[summary]
     
     Arguments:
@@ -62,20 +68,51 @@ def desenha_imagem(img, pontos_trajetoria, salva_imagem):
         salva_imagem {string} -- caminho que deseja salvar imagem (exemplo: /Documents/img.jpg) 
         
     """
-    print(pontos_trajetoria)
     altura, largura, _ = img.shape
     altura_da_parte = altura//len(pontos_trajetoria)
     centro_largura = largura//2 
-    imagem_desenhada = img[:] #criando uma cópia da imagem 
+    img_desenhada = img.copy() #criando uma cópia da imagem 
     for indice, ponto in enumerate(pontos_trajetoria):
-        imagem_desenhada = cv2.circle(imagem_desenhada, ponto, 5, (0,0,255), -1) #marcando os pontos da trajetória em verde
-        imagem_desenhada = cv2.circle(imagem_desenhada, (centro_largura,ponto[1]), 5, (255,0,0), -1) #marcando os pontos centrais em azul
-        imagem_desenhada = cv2.line(imagem_desenhada, (centro_largura,ponto[1]),(ponto),(0,255,0))
+        cv2.circle(img_desenhada, ponto, 5, (0,0,255), -1) #marcando os pontos da trajetória em verde
+        cv2.circle(img_desenhada, (centro_largura,ponto[1]), 5, (255,0,0), -1) #marcando os pontos centrais em azul
+        cv2.line(img_desenhada, (centro_largura,ponto[1]),(ponto),(0,255,0))
         distancia = (centro_largura+ponto[0]) #distancia entre ponto central e trajetoria
-        imagem_desenhada = cv2.putText(imagem_desenhada, str(abs(ponto[0]-centro_largura)), (distancia//2,ponto[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0))
-    cv2.imwrite(salva_imagem,imagem_desenhada)
+        cv2.putText(img_desenhada, str(abs(ponto[0]-centro_largura)), (distancia//2,ponto[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0))
+    cv2.imwrite(salva_imagem,img_desenhada)
+
+def desenha_faixa(img, pontos_trajetoria, salva_imagem, tamanho_linha, alpha):
+    """[summary]
+    
+    Arguments:
+        img {numpy.ndarray} -- imagem original de entrada
+        pontos_trajetoria {[list]} -- lista com os pontos da trajetoria
+        salva_imagem {string} -- caminho que deseja salvar imagem (exemplo: /Documents/img.jpg) 
+        
+    """
+    # print(pontos_trajetoria)
+    altura, largura, _ = img.shape
+    altura_da_parte = altura//len(pontos_trajetoria)
+    centro_largura = largura//2 
+    img_desenhada = img.copy() #criando uma cópia da imagem 
+    img_fundo = img.copy() #criando uma cópia da imagem 
+    for indice, ponto in enumerate(pontos_trajetoria):
+        # img_desenhada = cv2.circle(img_desenhada, ponto, 5, (0,0,255), -1) #marcando os pontos da trajetória em verde
+        # img_desenhada = cv2.circle(img_desenhada, (centro_largura,ponto[1]), 5, (255,0,0), -1) #marcando os pontos centrais em azul
+        # img_desenhada = cv2.line(img_desenhada, (centro_largura,ponto[1]),(ponto),(0,255,0))
+        # distancia = (centro_largura+ponto[0]) #distancia entre ponto central e trajetoria
+        # img_desenhada = cv2.putText(img_desenhada, str(abs(ponto[0]-centro_largura)), (distancia//2,ponto[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0))
+        # img_desenhada = cv2.line(img_desenhada, (centro_largura,ponto[1]),(ponto),(0,255,0))
+        cv2.line(img_desenhada, (ponto[0]-tamanho_linha,ponto[1]),(ponto[0]+tamanho_linha,ponto[1]),(0,255,0))
+    print(type(aplha))
+    img_sobreposta = cv2.addWeighted(img_fundo,alpha,img_desenhada,1-alpha,0)
+    cv2.imwrite(salva_imagem,img_sobreposta)
+    # cv2.imwrite("fundo.jpg",img_fundo)
+    
 
 
 
+tamanho_linha = args['tamanho_linha']
+n_pontos = args['n_pontos']
+alpha = args['alpha']
 
-processamento_imagem(caminho_imagem)
+processamento_imagem(caminho_imagem,n_pontos,tamanho_linha,alpha)
